631a457d0f559a3807af59e76f8d4d89227099d8
Sam Fields <sam.fields94@gmail.com>
2015-04-21 11:49:26 -0700

    P3 Update w/ Dijkstras

--- P3 NavMesh/p3_pathfinder.py
+++ P3 NavMesh/p3_pathfinder.py
@@ -16,45 +16,51 @@ def find_path (source_point, destination_point, mesh) :
 	dist = {}
 	
 	#set dist and prev for src
-	dist[source_point] = 0
-	prev[source_point] = None
+	dist[find_box(source_point, mesh)] = 0
+	prev[find_box(source_point, mesh)] = None
 	
-	#q is list of unvisited nodes
+	#q is list of unvisited boxes
 	q = []
 	
 	#push source_point onto q
-	heappush(q, (0,source_point))
+	heappush(q, (0,find_box(source_point,mesh)))
 	
+	destination_box = find_box(destination_point , mesh);
+	inDist = 0
+	altDist = 0
 	#Iterate through q
 	while q:
 		#This gets dist, then a node (i,j)
 		u = heappop(q)
-		#Throw away distance, store (i,j) in cell
+		#Throw away distance, store (x1,x2,y1,y2) in cell
 		_,cell = u
 		
 		#Go through all boxes, see which one our point is in
-		significant_box = find_box(cell, mesh)
-		
-		print "SIG BOX "
-		print significant_box
-		print "CELL "
-		print cell
+		#significant_box = find_box(cell, mesh)
+		# print "SIG BOX "
+		# print significant_box
+		# print "CELL "
+		# print cell
 				
 		#Get all the neighbors of that box
-		neighbors = mesh['adj'][significant_box]
+		neighbors = mesh['adj'][cell]
 		
 		#If destination, return
-		if source_point == destination_point:
-			return traceback(prev,u,source_point)
+		if cell == destination_box:
+			print "This doesn't happen"
+			return (traceback(prev,cell,source_point), visited)
 		for n in neighbors:
+		
+			visited.append(n)
 			#alt is the distance from source to this neighbor
-			next_point = find_border_point(cell,n)
-			alt = dist[cell] + distance(cell,next_point)
+			#next_point = find_border_point(cell,n)
+			alt = dist[cell] + distance(cell,n)
 			#If this is shorter than the previous shortest
-			if next_point not in dist or alt < dist[next_point]:
-				dist[next_point] = alt
-				prev[next_point] = cell
-				heappush(q,(alt,next_point))
+			
+			if n not in dist or alt < dist[n]:
+				dist[n] = alt
+				prev[n] = cell
+				heappush(q,(alt,n))
 	
 	return path, visited
 
@@ -67,17 +73,49 @@ def find_path (source_point, destination_point, mesh) :
 	
 	return path, visited"""
 	
+"""def traceback(prevlist, cell, src):
+	path = []
+	lastPoint = cell
+	cell = prevlist[cell];
+	while cell != None:
+		path.append((lastPoint, cell));
+		
+		cell = prevlist[cell];
+		lastPoint = cell
+		
+	return path"""
+	
 def traceback(prevlist, cell, src):
 	path = []
+	lastPoint = cell
+	lastPointMP = midpoint(lastPoint)
+	cell = prevlist[cell];
+	cellMP = midpoint(cell)
 	while cell != None:
-		path.append(cell);
+		cellMP = midpoint(cell)
+		path.append((lastPointMP, cellMP));
+		
+		lastPoint = cell
+		lastPointMP = midpoint(lastPoint)
 		cell = prevlist[cell];
+		
+	#print src
+	#path.append((lastPointMP,src))	
 	return path
 	
+def midpoint(box):
+	x = round((0.5*(box[0]+box[1])), 0)
+	y = round((0.5*(box[2]+box[3])), 0)
+	return (x,y)
+	
 def distance(cell1, cell2):
-	x1, y1 = cell1
-	x2, y2 = cell2
-	#Distance formula
+	x1 = 0.5*(cell1[0]+cell1[1])
+	y1 = 0.5*(cell1[2]+cell1[3])
+	x2 = 0.5*(cell2[0]+cell2[1])
+	y2 = 0.5*(cell2[2]+cell2[3])
+	# x1, y1 = cell1
+	# x2, y2 = cell2
+	# #Distance formula
 	return sqrt((x2-x1)**2 + (y2-y1)**2);
 	
 def find_box(point, mesh):
@@ -87,7 +125,7 @@ def find_box(point, mesh):
 	return None
 	
 def find_border_point(point, box):
-	resultx = point[0];
+	resultx = point[0]
 	resulty = point[1]
 	
 	if point[0] > box[1]:
@@ -98,7 +136,17 @@ def find_border_point(point, box):
 		resulty = box[3]
 	if point[1] < box[2]:
 		resulty = box[2]
-		
 	result = (resultx, resulty)
-		
 	return result
+	"""currX = point[0]
+	currY = point[1]
+	
+	borderX1 = box[0]
+	borderX2 = box[1]
+	borderY1 = box[2]
+	borderY2 = box[3]
+	
+	currX = min(borderX2-1,max(borderX1,currX))
+	currY = min(borderY2-1,max(borderY1,currY))
+		
+	return (currX, currY)"""
